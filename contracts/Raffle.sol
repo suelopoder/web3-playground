e5f8f5c891a7c385d9f7ee0fc474b4176c2f3465
Diego Sisto <diego.sisto@xapo.com>
2023-04-02 15:13:14 -0300

    Raffle end. Pick random winner.

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -7,6 +7,8 @@ import "@chainlink/contracts/src/v0.8/AutomationCompatible.sol";
 
 error Raffle__NotEnoughMoney();
 error Raffle__CantJoinWhileCalculatingWinner();
+error Raffle__TransferFailed(address payable winner, uint256 amount);
+error Raffle__UpkeepNotNeeded(uint256 players, uint256 state);
 
 enum Raffle_State {
     OPEN,
@@ -22,8 +24,12 @@ contract Raffle is VRFConsumerBaseV2, AutomationCompatibleInterface {
     uint32 private immutable i_callbackGasLimit;
     
     address payable[] private s_players;
+    address payable s_lastWinner;
     Raffle_State private s_state;
     uint256 private s_lastTimeStamp;
+
+    event WinnerPicked(address payable winner, uint256 amount);
+    event RequestedWinner(uint256 requestId);
 
     constructor(
         address vrfCoordinatorV2,
@@ -54,24 +60,29 @@ contract Raffle is VRFConsumerBaseV2, AutomationCompatibleInterface {
         s_players.push(payable(msg.sender));
     }
 
+    // Got dice results!
     function fulfillRandomWords(
-        uint256 requestId,
+        uint256,
         uint256[] memory randomWords
     ) internal override {
-        // TODO called when we got a random response
-        // s_lastTimeStamp = block.timestamp;
-        // s_players = new address payable[](0);
-        // s_raffleState = RaffleState.OPEN;
+        uint256 indexOfWinner = randomWords[0] % s_players.length;
+        address payable winner = s_players[indexOfWinner];
+        s_lastWinner = winner;
+        s_lastTimeStamp = block.timestamp;
+        s_players = new address payable[](0);
+        s_state = Raffle_State.OPEN;
+
+        // Send money to winner
+        uint256 amount = address(this).balance;
+        (bool success, ) = winner.call{value: amount}("");
+
+        if (!success) {
+            revert Raffle__TransferFailed(winner, amount);
+        }
+        emit WinnerPicked(winner, amount);
     }
 
-    function getPlayer(uint256 index) public view returns (address) {
-        return s_players[index];
-    }
-
-    function getPlayerCount() public view returns (uint256) {
-        return s_players.length;
-    }
-
+    // can current raffle end?
     function canClose() public view returns (bool) {
         bool isOpen = s_state == Raffle_State.OPEN;
         bool hasPlayers = s_players.length > 0;
@@ -79,6 +90,7 @@ contract Raffle is VRFConsumerBaseV2, AutomationCompatibleInterface {
         return isOpen && hasPlayers && timePassed;
     }
 
+    // chainlink checking if it needs to call perform upkeep
     function checkUpkeep(
         bytes calldata
     )
@@ -91,16 +103,32 @@ contract Raffle is VRFConsumerBaseV2, AutomationCompatibleInterface {
         return (upkeepNeeded, "0x0");
     }
 
+    // chainlink letting us now it's time to check if we need to end raffle
     function performUpkeep(bytes calldata) external override {
-        require(canClose(), "Upkeep not needed");
+        bool close = canClose();
+        if (!close) {
+            revert Raffle__UpkeepNotNeeded(
+                s_players.length,
+                uint256(s_state)
+            );
+        }
 
         s_state = Raffle_State.CALCULATING;
-        i_vrfCoordinator.requestRandomWords(
+        uint256 requestId = i_vrfCoordinator.requestRandomWords(
             i_gasLane,
             i_subscriptionId,
             1,
             i_callbackGasLimit,
             1
         );
+        emit RequestedWinner(requestId);
+    }
+
+    function getPlayer(uint256 index) public view returns (address) {
+        return s_players[index];
+    }
+
+    function getPlayerCount() public view returns (uint256) {
+        return s_players.length;
     }
 }
--- test/Raffle.ts
+++ test/Raffle.ts
@@ -1,6 +1,7 @@
 import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 import { expect } from "chai";
-import { ethers } from "hardhat";
+import { BigNumber } from "ethers";
+import { ethers, network } from "hardhat";
 
 const BASE_FEE = "250000000000000000" // 0.25 is this the premium in LINK?
 const GAS_PRICE_LINK = 1e9 // link per gas, is this the gas lane? // 0.000000001 LINK per gas
@@ -14,23 +15,30 @@ describe("Raffle", function () {
     const [owner, ...accounts] = await ethers.getSigners();
 
     // if (['localhost', 'hardhat'].includes(network.name) {
-    const VRFCoordMock = await ethers.getContractFactory("VRFCoordinatorV2Mock")
+    const VRFCoordMock = await ethers.getContractFactory("VRFCoordinatorV2Mock", owner)
     const vrfCoordinatorV2Mock = await VRFCoordMock.deploy(BASE_FEE, GAS_PRICE_LINK)
 
     console.log('VRFCoordinatorV2Mock deployed at', vrfCoordinatorV2Mock.address)
-
+    
     const transactionResponse = await vrfCoordinatorV2Mock.createSubscription()
-    const transactionReceipt = await transactionResponse.wait()
+    const transactionReceipt = await transactionResponse.wait(1)
     const subscriptionId = transactionReceipt.events[0].args.subId
-
+    
     const gasLane = "0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c" // 30 gwei
     const callbackGasLimit = "500000" // 500,000 gas
     const keepersUpdateInterval = "30" // in seconds
     
     const Raffle = await ethers.getContractFactory("Raffle");
     const raffle = await Raffle.deploy(vrfCoordinatorV2Mock.address, subscriptionId, gasLane, callbackGasLimit, keepersUpdateInterval);
-
-    return { raffle, owner, accounts };
+    console.log('Raffle deployed at', raffle.address)
+    
+    await vrfCoordinatorV2Mock.fundSubscription(
+      subscriptionId,
+      ethers.utils.parseEther("30")
+    );
+    await vrfCoordinatorV2Mock.addConsumer(subscriptionId, raffle.address)
+    
+    return { raffle, owner, accounts, vrfCoordinatorV2Mock, subscriptionId };
   }
 
   describe("Deployment", function () {
@@ -41,7 +49,7 @@ describe("Raffle", function () {
     });
   });
 
-  describe("Join", function() {
+  describe("Join", function () {
     it('Should fail when no money is passed', async () => {
       const { raffle } = await loadFixture(deploy);
       expect(raffle.join()).to.be.revertedWith("Raffle__NotEnoughMoney")
@@ -64,7 +72,52 @@ describe("Raffle", function () {
     })
   })
 
-  describe("Winners", () => {
-    // TODO
+  describe("Raffle end", () => {
+    it('Should pick a winner eventually', async () => {
+      const { owner, raffle, accounts, vrfCoordinatorV2Mock, subscriptionId } = await loadFixture(deploy);
+      await raffle.connect(accounts[1]).join({ value: 1 })
+      await raffle.connect(accounts[2]).join({ value: 1 })
+      await raffle.connect(accounts[3]).join({ value: 1 })
+      // console.log('Players:', accounts[0].address, accounts[1].address, accounts[2].address)
+      const balancesBefore = [await accounts[0].getBalance(), await accounts[1].getBalance(), await accounts[2].getBalance()]
+      await raffle.connect(owner)
+
+      // Magically push time forward
+      await network.provider.send("evm_increaseTime", [31])
+      await network.provider.request({ method: "evm_mine", params: [] })
+
+      await new Promise<void>(async (resolve, revert) => {
+        try {
+          raffle.once('WinnerPicked', async (winnerAddress: string, balance: BigNumber) => {
+            // console.log('Winner Picked', winnerAddress, balance.toNumber())
+
+            for (let i = 1; i <= 3; i++) {
+              const currentAccount = accounts[i]
+              if (currentAccount.address === winnerAddress) {
+                const balanceNow = await currentAccount.getBalance()
+                const balanceBefore = balancesBefore[i - 1]
+                expect(balanceNow).to.be.greaterThan(balanceBefore)
+                // TODO check the exact amount won. Consider fees!
+                resolve()
+                return
+              }
+            }
+
+            revert(new Error('Winner not found'))
+          })
+
+          // Mock chainlink external call
+          const tx = await raffle.performUpkeep("0x")
+          await tx.wait(1)
+
+          await vrfCoordinatorV2Mock.fulfillRandomWords(
+            subscriptionId,
+            raffle.address,
+          )
+        } catch (e) {
+          revert(e)
+        }
+      })
+    })
   })
 });
